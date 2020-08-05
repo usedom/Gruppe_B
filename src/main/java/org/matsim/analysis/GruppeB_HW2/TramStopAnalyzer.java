3b1e0c5df1a091b434dc5b9d6c833104e0aa25d7
Julia <j.s.gebauer@web.de>
2020-08-05 15:04:53 +0200

    Working TramStopAnalyzer with person ID list of people using M10

--- src/main/java/org/matsim/analysis/GruppeB_HW2/TramStopAnalyzer.java
+++ src/main/java/org/matsim/analysis/GruppeB_HW2/TramStopAnalyzer.java
@@ -1,147 +1,158 @@
 package org.matsim.analysis.GruppeB_HW2;
 
 import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.events.LinkEnterEvent;
-import org.matsim.api.core.v01.events.PersonDepartureEvent;
+import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.events.PersonEntersVehicleEvent;
-import org.matsim.api.core.v01.events.handler.LinkEnterEventHandler;
-import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonEntersVehicleEventHandler;
-import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.core.api.experimental.events.AgentWaitingForPtEvent;
 import org.matsim.core.api.experimental.events.handler.AgentWaitingForPtEventHandler;
+import org.matsim.pt.transitSchedule.api.TransitLine;
+import org.matsim.pt.transitSchedule.api.TransitRoute;
 import org.matsim.pt.transitSchedule.api.TransitStopFacility;
-import org.matsim.vehicles.Vehicle;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
-public class TramStopAnalyzer implements PersonEntersVehicleEventHandler, LinkEnterEventHandler, PersonDepartureEventHandler, AgentWaitingForPtEventHandler {
+public class TramStopAnalyzer implements PersonEntersVehicleEventHandler, AgentWaitingForPtEventHandler {
    /*Je nachdem welchen Input ich gebe, soll der Tramstopanalyzer eine txt. Datei ausgeben, in dem drin steht wie viele
-     Personen an welcher Haltestelle eingestiegen sind. Vllt auch nochmal zeitlicher Verlauf für den Tag*/
+     Personen an welcher Haltestelle eingestiegen sind.*/
     private BufferedWriter bufferedWriter;
 
-    List<Id<Link>> linksM10= new ArrayList<>();
-    List<Id<Vehicle>> vehiclesM10= new ArrayList<>();
-    List<Id<Person>> FahrgastTram = new ArrayList<>();
-    List<Id<Person>> FahrerTram = new ArrayList<>();
-    String expVehLW = "pt_M10---17440_900_14_29";
-    String expVehWL = "pt_M10---17440_900_7_31";
-    List<Id<Link>> tramstops = new ArrayList<>();
-    Map<Id<Link>, Integer> auslastung_Haltestellen = new HashMap<>();
-    Integer count = 0;
-    Map<Id<Person>,Id<Link>> test =new HashMap<>();
-    List<Id<Vehicle>> tram10veh = new ArrayList<>();
-    List<Id<TransitStopFacility>> testStops = new ArrayList<>() ;
-    List<Id<Person>> singlePerson = new ArrayList<>();
+    Scenario scenario;
 
-    public TramStopAnalyzer(String output, List<Id<Vehicle>> fahrzeuge){
-        tram10veh.addAll(fahrzeuge);
-        try {
+    List<Id<TransitRoute>> tramM10routeid = new ArrayList<>();
+    Set<Id<TransitStopFacility>> M10Haltestellen = new HashSet();
+    List<Id<TransitStopFacility>> listM10Haltestellen = new ArrayList<>();
+    List<Integer> checkRoutelaenge = new ArrayList<>();
+    List<Id<Person>> listpersonentersM10 = new ArrayList<>();
+    Set<Id<Person>> setpersonentersM10 = new HashSet<>();
+    Map<Id<TransitStopFacility>, Integer> auslastungM10 = new HashMap<>();
+    List<String> stringlist = new ArrayList<>();
+    Integer count_her = 0;
+    Integer count_pfl = 0;
+    Integer count_gor = 0;
+    Integer count_fal = 0;
+
+
+
+    public TramStopAnalyzer(Scenario scenario, String output){
+        this.scenario = scenario;
+        List<Id<TransitRoute>> trlineM10 = new ArrayList<>(scenario.getTransitSchedule().getTransitLines().get(Id.create("M10---17440_900", TransitLine.class)).getRoutes().keySet());
+
+       //Haltestellen nodes der M10 finden
+       // scenario.getTransitSchedule().getTransitLines().get(Id.create("M10---17440_900", TransitLine.class)).getRoutes().get().getStop().getStopFacility();
+
+       for (int i = 0; i < trlineM10.size(); i++){
+            if (String.valueOf(trlineM10.get(i)).contains("M10")){
+                tramM10routeid.add(trlineM10.get(i));
+            }
+        }
+
+       for (Id<TransitRoute> id:tramM10routeid){
+           int routelaenge = scenario.getTransitSchedule().getTransitLines().get(Id.create("M10---17440_900", TransitLine.class)).getRoutes().get(id).getStops().size();
+            checkRoutelaenge.add(routelaenge);
+           for (int i = 0; i < routelaenge; i++){
+               M10Haltestellen.add(scenario.getTransitSchedule().getTransitLines().get(Id.create("M10---17440_900", TransitLine.class)).getRoutes().get(id).getStops().get(i).getStopFacility().getId());
+               stringlist.add(scenario.getTransitSchedule().getTransitLines().get(Id.create("M10---17440_900", TransitLine.class)).getRoutes().get(id).getStops().get(i).getStopFacility().getId().toString());
+           }
+       }
+
+       try {
             FileWriter fileWriter = new FileWriter(output);
             bufferedWriter = new BufferedWriter(fileWriter);
         } catch(IOException ee){
             throw new RuntimeException(ee);
         }
     }
-    //UNNÖTIG! KANN WEG!
-    @Override
-    public void handleEvent(LinkEnterEvent vehicle_enters_link){
-       if (vehicle_enters_link.getVehicleId().equals(Id.create(expVehLW,expVehLW.getClass()))||vehicle_enters_link.getVehicleId().equals(Id.create(expVehWL,expVehWL.getClass()))) {
-            linksM10.add(vehicle_enters_link.getLinkId());
-            auslastung_Haltestellen.put(vehicle_enters_link.getLinkId(),0);
-        }
-        //Holt die dazugehörigen Fahrzeuge raus, ABER das sind noch alle pt auf dem link
-       for (Id<Link> linkid:linksM10) {
-            if (vehicle_enters_link.getLinkId().equals(linkid)) {
-                if (!vehiclesM10.contains(vehicle_enters_link.getVehicleId())) {
-                    //Aus der Liste mit allen pt für die links, werden die pt_M10 Einträge sortiert
-                   if (String.valueOf(vehicle_enters_link.getVehicleId()).contains("pt_M10")) {
-                        vehiclesM10.add(vehicle_enters_link.getVehicleId());
-                   }
-                }
-            }
-       }
-    }
 
     @Override
     public void handleEvent(PersonEntersVehicleEvent person_enters_tram){
-
-        for(Id<Vehicle> vehid:tram10veh){
-            if (person_enters_tram.getVehicleId().compareTo(vehid)==0){
+        Set<Id<Person>> allpersonidsveh = scenario.getPopulation().getPersons().keySet();
+        for (Id<Person> id:allpersonidsveh){
+            if (person_enters_tram.getPersonId().equals(id)){
                 if (!String.valueOf(person_enters_tram.getPersonId()).contains("pt_pt")){
-                FahrgastTram.add(person_enters_tram.getPersonId());
-                } else {
-                    //Die Liste ist quasi unnötig weil es für jedes Fahreug quasi einen Fahrer gibt
-                    FahrerTram.add(person_enters_tram.getPersonId());
+                    if (String.valueOf(person_enters_tram.getVehicleId()).contains("pt_M10")){
+                        setpersonentersM10.add(person_enters_tram.getPersonId());
+                        listpersonentersM10.add(person_enters_tram.getPersonId());
+                    }
                 }
-
             }
         }
-    }
-    //UNNÖTIG! KANN WEG!
-    @Override
-    public void handleEvent(PersonDepartureEvent person_haltestelle_einstieg){
-        for (Id<Person> p:FahrgastTram){
-           if (person_haltestelle_einstieg.getPersonId().equals(p) && String.valueOf(linksM10).contains(String.valueOf(person_haltestelle_einstieg.getLinkId())) && person_haltestelle_einstieg.getLegMode().equals("pt")){
-               tramstops.add(person_haltestelle_einstieg.getLinkId());
-               if (String.valueOf(auslastung_Haltestellen.keySet()).contains(String.valueOf(person_haltestelle_einstieg.getLinkId()))){
-                   auslastung_Haltestellen.put(person_haltestelle_einstieg.getLinkId(), auslastung_Haltestellen.get(person_haltestelle_einstieg.getLinkId())+1);
-                   count=count+1;
-                   test.put(person_haltestelle_einstieg.getPersonId(),person_haltestelle_einstieg.getLinkId());
-               }
-           }
-        }
-
     }
 
     @Override
     public void handleEvent(AgentWaitingForPtEvent person_wartet_Haltestelle){
-        Set<Id<Person>> einzelne_Personen = new HashSet<>(FahrgastTram);
+     /*   for (Id<TransitStopFacility> id:M10Haltestellen) {
+           if (person_wartet_Haltestelle.getWaitingAtStopId().equals(id)) {
+               if (auslastungM10.keySet().contains(id)){
+                   auslastungM10.put(id,auslastungM10.get(id)+1);
+               }
+               if (!auslastungM10.keySet().contains(id)){
+                   auslastungM10.put(id,1);
+               }
+           }
+       }*/
+        for (Id<TransitStopFacility> id:M10Haltestellen) {
+            if (person_wartet_Haltestelle.getWaitingAtStopId().equals(id)) {
+                if (id.equals(Id.create("070101004244.9", TransitStopFacility.class)) || id.equals(Id.create("070101004244.8", TransitStopFacility.class))){
+                    //Hermannplatz
+                    count_her=count_her+1;
+                }
+                if (id.equals(Id.create("070101003213.9", TransitStopFacility.class)) || id.equals(Id.create("070101003213.8", TransitStopFacility.class))){
+                    //Pflüger str
+                    count_pfl=count_pfl+1;
+                }
+                if (id.equals(Id.create("07010100newpark.8", TransitStopFacility.class)) || id.equals(Id.create("07010100newpark.9", TransitStopFacility.class))){
+                    //Görlitzer Park
+                    count_gor=count_gor+1;
+                }
+                if (id.equals(Id.create("070101001365.8", TransitStopFacility.class)) || id.equals(Id.create("070101001365.9", TransitStopFacility.class))){
+                    //Falckensteiner Str.
+                    count_fal=count_fal+1;
+                }
 
-        List<Id<Person>> testlistperson = new ArrayList<>();
-        testlistperson.add(Id.createPersonId("234372001"));
-        testlistperson.add(Id.createPersonId("360285601"));
-//einzelne_Personen
-        for (Id<Person> pid:testlistperson){
-            if (person_wartet_Haltestelle.getPersonId().equals(pid)){
-                testStops.add(person_wartet_Haltestelle.getWaitingAtStopId());
             }
         }
-       // System.out.println("Anzahl einzelne Personen" + einzelne_Personen.size()+ "Anzahl Fahrten" + personTram.size());
+
+
     }
-    //Was ist gegeben: es sind die FahrzeugIDs bekannt und die Personen, die einsteigen.
-    /*Was noch gesucht wird: Aus den events suche ich die stopFacilities raus an denen die Personen warten
-    zum einsteigen. wenn daraufhin die nächste activity/event "person enters vehicle --> pt_M10...,
-    wird der name für die stopFacility aus transitschedule geholt und in der Haltestellen Auslastung der
-    counter hochgesetzt.*/
 
     public void print(){
+        List<Id<Person>> person_entering = new ArrayList<>(setpersonentersM10);
+        /*In der Text datei wird der Haltestellen Name, die Anzahl der Personen die einsteigen sowie die Anzahl an
+        Fahrten die gemacht werden, und die Anzahl an Personen, die diese Fahrten machen*/
         System.out.println("###WICHTIGER PART###");
-        System.out.println("Anzahl Fahrgäste" + FahrgastTram.size() + "und Anzahl Mitarbeiter" + FahrerTram.size());
+        System.out.println("Anzahl an Fahrgästen M10 " +setpersonentersM10.size() + " Anzahl an Fahrten" + listpersonentersM10.size());
+        System.out.println("Einstieg Hermannplatz " + count_her );
+        System.out.println("Einstieg Pflügerstr. " + count_pfl );
+        System.out.println("Einstieg Görtlitzer Park " + count_gor );
+        System.out.println("Einstieg Falckensteinstr. " + count_fal );
         System.out.println("### DONE ###");
-        try {
-          /*  bufferedWriter.write("Haltestelle\tAuslastung" );
-            bufferedWriter.newLine();
-            for (int i = 0; i < auslastung_Haltestellen.keySet().size(); i++) {
-                bufferedWriter.write(linksM10.get(i) + "\t" + auslastung_Haltestellen.get(linksM10.get(i)));
-                bufferedWriter.newLine();
-            }
-            bufferedWriter.write("Summe der Personen in total:" + personTram.size() + "gegenchecken mit warten auf pt" + testStops.size()+ "und wie viele einzelne Personen sind es" +singlePerson.size());
-            bufferedWriter.close();*/
-            bufferedWriter.write("Personen die in M10 steigen = " + FahrgastTram.size());
-            bufferedWriter.newLine();
-            for (int i = 0; i < FahrgastTram.size(); i++) {
-                bufferedWriter.write(String.valueOf(FahrgastTram.get(i)));
-                bufferedWriter.newLine();
-            }
+
+      try {
+        /* bufferedWriter.write("Anzahl an Fahrgästen M10 " +setpersonentersM10.size() + " Anzahl an Fahrten" + listpersonentersM10.size());
+         bufferedWriter.newLine();
+         bufferedWriter.write("neue Haltestellen Auslastung" );
+         bufferedWriter.newLine();
+         bufferedWriter.write("Einstieg Hermannplatz " + count_her );
+         bufferedWriter.newLine();
+         bufferedWriter.write("Einstieg Pflügerstr. " + count_pfl );
+         bufferedWriter.newLine();
+         bufferedWriter.write("Einstieg Görtlitzer Park " + count_gor );
+         bufferedWriter.newLine();
+         bufferedWriter.write("Einstieg Falckensteinstr. " + count_fal );
+         bufferedWriter.newLine();*/
+         for(int i=0; i<person_entering.size(); i++){
+             bufferedWriter.write(String.valueOf(person_entering.get(i)));
+             bufferedWriter.newLine();
+         }
+
+         bufferedWriter.close();
         } catch (IOException ee){
             throw new RuntimeException(ee);
         }
-
     }
-
 }
